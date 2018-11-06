d4a0afcd23b4b7b8207e29b8f3ec348003dd6706
DanCurrie15 <danjc_15@hotmail.com>
2018-11-05 23:13:57 -0500

    fix: multiple
    -added lang="en" to both .html files
    -added aria-labels to breadcrumb and maps
    -corrected semantic importance of headings on restaruant details page
    -moved sw.js to top level folder so it works on local host
    -added to fetch function now clones the request

--- sw.js
+++ sw.js
@@ -31,17 +31,38 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-        .then(function(response) {
-            if(response){
-                return response
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // Clone the request
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
             }
-            // not in cache, return from network
-            return fetch(event.request, {credentials: 'include'});
-        })
+
+            // Clone the response
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
     );
-})
+});
 
 self.addEventListener('activate', function(event) {
 
